1449683de0dfe271653f24a05cbc6adf6c12be32
Dennis José da Silva <dennisjosesilva@gmail.com>
2024-09-06 15:59:14 -0300

    Runtime data for the lastest Paulo optimisation.

---
+++ runtime-analysis/csv3/group_data.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+import os
+from os.path import isfile, join
+import numpy as np
+
+def main(RESOLUTION="1920x1080"):
+
+  def extract_mean_runtime(path):        
+    lfiles = [f for f in os.listdir(path) if isfile(join(path, f))]
+    dfs = []
+    
+    for f in lfiles:
+      dfs.append(pd.read_csv(join(path, f), sep=";"))
+
+    # compute mean runtime
+    runtime_col = sum(df["runtime"] for df in dfs) / len(dfs)
+    
+    return runtime_col 
+
+  
+
+  non_diff_runtime = extract_mean_runtime(join("non_diff", f"{RESOLUTION}"))
+  diff_max_runtime = extract_mean_runtime(join("max_diff", f"{RESOLUTION}"))
+  exact_edt_runtime = extract_mean_runtime(join("exact_edt", f"{RESOLUTION}"))
+  diff_max_dist_opt2_runtime = extract_mean_runtime(join("diff_max_dist_opt2", f"{RESOLUTION}"))
+  diff_max_dist_opt3_runtime = extract_mean_runtime(join("diff_max_dist_opt3", f"{RESOLUTION}"))
+
+  df = pd.read_csv(join("non_diff", f"{RESOLUTION}", "runtime_1.csv"), sep=";")
+  df = df.drop(columns=["runtime"])
+
+  df = df.assign(
+    **{ "runtime_non_diff": non_diff_runtime ,
+      "runtime_exact_edt": exact_edt_runtime ,
+      "runtime_max_diff": diff_max_runtime ,      
+      "runtime_max_dist_opt2": diff_max_dist_opt2_runtime,
+      "runtime_max_dist_opt3": diff_max_dist_opt3_runtime,      
+      "speedup_non_diff": non_diff_runtime / diff_max_dist_opt3_runtime, 
+      "speedup_exact": exact_edt_runtime / diff_max_dist_opt3_runtime,
+      "speedup_max_diff": diff_max_runtime / diff_max_dist_opt3_runtime,
+      "speedup_max_dist_opt2": diff_max_dist_opt2_runtime / diff_max_dist_opt3_runtime,
+      })
+
+  df.to_csv(f"mean_runtime_{RESOLUTION}.csv", sep=";")
+
+parser = ArgumentParser(description="Generate mean runtime data",
+                        formatter_class=ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("-r", "--resolution", 
+                    choices=["1920x1080", "960x540", "480x270", "240x135", "120x68"],
+                    default="1920x1080")
+
+args = parser.parse_args()
+
+
+main(RESOLUTION=args.resolution)
---
+++ runtime-analysis/run_gen_csv2_diff_max_dist_opt3.sh
@@ -0,0 +1,45 @@
+#!/bin/bash 
+
+# options
+options=("1920x1080" "960x540" "480x270" "240x135" "120x68" "quit")
+REL=""
+
+echo "choose a resolution option:"
+select opt in "${options[@]}"
+do 
+  # Save the selected option directly
+  REL="$opt"
+
+  # Check if the user select "Quit" or an invalid option
+  if [ "$opt" == "quit" ]; then 
+    echo "Exiting..."
+    break
+  elif [ -z "$opt" ]; then 
+    echo "Invalid option $REL. Please choose a valid number."
+  else
+    echo "You selected: $REL"
+    break
+  fi
+done
+
+
+Define the directory name 
+DIR="csv2/diff_max_dist_opt3/$REL"
+
+# Check if the directory exists
+if [ -d "$DIR" ]; then 
+  echo "Directory $DIR exists. Removing it."
+  rm -rf "$DIR"
+fi 
+
+# Create the directory
+mkdir -p "$DIR"
+echo "Directory $DIR created"
+
+# Run experiments
+python gen_csv2.py -i "../runtime-dataset/dataset-icdar/$REL/validation" -o "$DIR/runtime_1.csv" -p "../build/diff_max_dist_opt3_runtime"
+sleep 2
+python gen_csv2.py -i "../runtime-dataset/dataset-icdar/$REL/validation" -o "$DIR/runtime_2.csv" -p "../build/diff_max_dist_opt3_runtime"
+sleep 2
+python gen_csv2.py -i "../runtime-dataset/dataset-icdar/$REL/validation" -o "$DIR/runtime_3.csv" -p "../build/diff_max_dist_opt3_runtime"
+
